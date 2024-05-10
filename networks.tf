7c213408154e6d79de4ca3dbbb0615be7662d879
duha2060 <maksimov2060@gmail.com>
2024-05-10 16:32:04 +0300

    add app.tf/ mod networks.tf / del app01.tf app02.tf

--- networks.tf
+++ networks.tf
@@ -28,3 +28,29 @@ resource "yandex_vpc_subnet" "subnet-public" {
   v4_cidr_blocks = ["192.168.3.0/24"]
   network_id = "${yandex_vpc_network.network-1.id}"
 }
+
+
+resource "yandex_alb_backend_group" "backend-group" {
+  name = "backend-group"
+
+  http_backend {
+    name             = "backend"
+    weight           = 1
+    port             = 80
+    target_group_ids = [yandex_alb_target_group.web.id]
+
+    load_balancing_config {
+      panic_threshold = 90
+    }
+
+    healthcheck {
+      timeout             = "15s"
+      interval            = "2s"
+      healthy_threshold   = 10
+      unhealthy_threshold = 15
+      http_healthcheck {
+        path = "/"
+      }
+    }
+  }
+}
