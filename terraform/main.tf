054e1fdc4b1a7d28222be5cf658dda1bb640b804
duha2060 <maksimov2060@gmail.com>
2024-05-26 15:06:36 +0300

    chg main

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  token = "y0_AgAAAAAgmCVfAATuwQAAAADwls3fFTlvS0ksQHuj6uVksPFeH8Goqq4"
-  cloud_id = "b1ga3ko2a8tfk5f7tbh7"
-  folder_id = "b1g8e5dbjk7p1t1o2hnr"
-  zone = "ru-central1-a"
+  token = ""
+  cloud_id = ""
+  folder_id = ""
+  zone = ""
 }
